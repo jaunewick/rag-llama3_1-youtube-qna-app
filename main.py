424073b5f9b96af12ef0ba41c8c54e0fade526d2
Daniel Giao <daniel.giao@icloud.com>
2024-08-26 04:03:25 -0400

    Delete video endpoint handled and questions/answers aggregate

--- main.py
+++ main.py
@@ -38,31 +38,50 @@ async def create_video(video: VideoBase, db: db_dependency):
 async def read_video(video_id: int, db: db_dependency):
     video = db.query(models.Video).filter(models.Video.id == video_id).first()
     if video is None:
-        HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Video not found")
-    return video
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Video not found")
+    questions = db.query(models.Question).filter(models.Question.video_id == video_id).all()
+    questions_and_answers = []
+    for question in questions:
+        answer = db.query(models.Answer).filter(models.Answer.question_id == question.id).first()
+        questions_and_answers.append({
+            "question": question,
+            "answer": answer
+        })
+    return {
+        "video": video,
+        "questions_and_answers": questions_and_answers
+    }
+
+@app.delete("/videos/{video_id}", status_code=status.HTTP_200_OK)
+async def delete_video(video_id: int, db: db_dependency):
+    video = db.query(models.Video).filter(models.Video.id == video_id).first()
+    if video is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Video not found")
+
+    questions = db.query(models.Question).filter(models.Question.video_id == video_id).all()
+    for question in questions:
+        answer = db.query(models.Answer).filter(models.Answer.question_id == question.id).first()
+        if answer:
+            db.delete(answer)
+        db.delete(question)
+    db.delete(video)
+    db.commit()
+    return {"message": "Video deleted successfully"}
+
 
 @app.post("/questions/", status_code=status.HTTP_201_CREATED)
 async def create_question(question: QuestionBase, db: db_dependency):
     db_question = models.Question(**question.model_dump())
     db.add(db_question)
     db.commit()
-
-@app.get("/questions/{question_id}", status_code=status.HTTP_200_OK)
-async def read_question(question_id: int, db: db_dependency):
-    question = db.query(models.Question).filter(models.Question.id == question_id).first()
-    if question is None:
-        HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Question not found")
-    return question
-
-@app.post("/answers/", status_code=status.HTTP_201_CREATED)
-async def create_answer(answer: AnswerBase, db: db_dependency):
-    db_answer = models.Answer(**answer.model_dump())
+    answer_text = "blah"
+    db_answer = models.Answer({
+        "question_id": db_question.id,
+        "answer_text": answer_text
+    })
     db.add(db_answer)
     db.commit()
-
-@app.get("/answers/{answer_id}", status_code=status.HTTP_200_OK)
-async def read_answer(answer_id: int, db: db_dependency):
-    answer = db.query(models.Answer).filter(models.Answer.id == answer_id).first()
-    if answer is None:
-        HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Answer not found")
-    return answer
+    return {
+        "question": db_question,
+        "answer": db_answer
+    }
