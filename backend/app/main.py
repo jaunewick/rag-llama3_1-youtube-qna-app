c3bec16f08ce96081ee5f36fbbd9c5617b1720cc
Daniel Giao <daniel.giao@icloud.com>
2025-09-02 16:57:04 -0400

    Added CORS config in backend and updated delete_video logic

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import backend.app.models as models
 from backend.app.database import engine
 from backend.app.routers import question_router, video_router
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 app.include_router(question_router.router)
 app.include_router(video_router.router)
